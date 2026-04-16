157e401db91061279a2ea7329523bbb1b033afda
srikartankala <srikartankala@gmail.com>
2026-04-16 10:22:17 +0530

    Srikar Added MapBogie

---
+++ TrainApp/src/TrainConsistManagementApp.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println(" UC6 - Map Bogie to Capacity (HashMap) ");
+        System.out.println("=======================================\n");
+
+        Map<String, Integer> capacityMap = new HashMap<>();
+
+        capacityMap.put("First Class", 24);
+        capacityMap.put("Cargo", 120);
+        capacityMap.put("Sleeper", 72);
+        capacityMap.put("AC Chair", 56);
+
+        System.out.println("Bogie Capacity Details:");
+
+        for (Map.Entry<String, Integer> entry : capacityMap.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+
+        System.out.println("\nUC6 bogie-capacity mapping completed...");
+    }
+}
