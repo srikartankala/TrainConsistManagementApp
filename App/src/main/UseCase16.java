e1b89b8643d7d1c960ba9b9269ebff97c00ec0cb
srikartankala <srikartankala@gmail.com>
2026-04-24 10:39:47 +0530

    srikar added UC16

---
+++ App/src/main/UseCase16.java
@@ -0,0 +1,27 @@
+public class UseCase16 {
+
+    public static void main(String[] args) {
+
+        int[] capacities = {72, 56, 24, 70, 60};
+
+        System.out.println("Original Capacities:");
+        for (int c : capacities) {
+            System.out.print(c + " ");
+        }
+
+        for (int i = 0; i < capacities.length - 1; i++) {
+            for (int j = 0; j < capacities.length - i - 1; j++) {
+                if (capacities[j] > capacities[j + 1]) {
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j + 1];
+                    capacities[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("\nSorted Capacities:");
+        for (int c : capacities) {
+            System.out.print(c + " ");
+        }
+    }
+}
